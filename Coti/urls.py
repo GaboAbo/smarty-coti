e3e6e32173cd2b1216b81345f8cd4457fa14458d
GaboAbo <robertgabrielmendozatovar@gmail.com>
2025-05-27 17:32:29 -0400

    tweaks 2

--- Coti/urls.py
+++ Coti/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('App.urls')),
+    path('authUser/', include('AuthUser.urls')),
 ]
